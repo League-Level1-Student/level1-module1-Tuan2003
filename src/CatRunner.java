8bfd9fae0465b84f0ec49ca3c55bc6d00b5b6e9e
313509 <313509@0338S-S-R0B6H0H.sdcs.local>
2019-02-19 14:25:15 -0800

    CatRunner, HarryPotterRunner, Platypus, PlatypusRunner, RaceCarRunner

---
+++ src/CatRunner.java
@@ -0,0 +1,9 @@
+
+public class CatRunner {
+public static void main(String[] args) {
+	Cat cat = new Cat("Erick Lamont Fentress");
+	cat.meow();
+	cat.printName();
+	cat.kill();
+}
+}
--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,22 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
-		// 2. Print the RaceCar's position in the race
-
-		// 3. Crash the RaceCar
 		
+		RaceCar racecar = new RaceCar("Car",5);
+		
+		// 2. Print the RaceCar's position in the race
+		
+System.out.println("The car's position is: " + racecar.getPositionInRace());
+		// 3. Crash the RaceCar
+racecar.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if(racecar.isDamaged() == true) {
+	racecar.pit();
+}
 		// 5. Help the car move into first place.
-
+while(racecar.getPositionInRace()>1) {
+	racecar.overtake();
+	
+		}
 	}
 }
